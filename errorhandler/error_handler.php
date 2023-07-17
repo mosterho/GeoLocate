5282aa5b9b1ef018be13a51e203bd9ba2b660a66
Marty <mosterho@ramapo.edu>
2023-07-17 18:04:51 -0400

    Quick update
    Remove one file, quick code update

--- errorhandler/error_handler.php
+++ errorhandler/error_handler.php
@@ -2,11 +2,9 @@
 //
 function logmessage($arg_message='A default message') {
 
-  // Build a message that mimics the syslog format.
+  // Build a message and utilize the syslog function.
   // https://datatracker.ietf.org/doc/html/rfc5424#section-6.2.1
   // Determine message priority "prival" based on syslog format. Facility "LOCAL0" and severity "notice".
-
-  #$dt = date(DATE_ISO8601);
 
   $my_json = file_get_contents("/var/www/Geolocate/errorhandler/error_handler.json");
   $my_decoded_json = json_decode($my_json);
@@ -27,9 +25,6 @@ function logmessage($arg_message='A default message') {
   openlog('Geolocate', LOG_PID, LOG_LOCAL0);
   syslog(LOG_NOTICE, $full_message);
   closelog();
-
-  #error_log($full_message, 3, $arg_logname);
 }
-
 
 ?>
